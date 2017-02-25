3c1f8bbfa7c6dc27853a3966b50733fcbe36a5ad
PeterTheDestroyer <peter.lee@berkeley.edu>
2017-02-25 13:01:51 -0800

    Fixed indentation

--- sherlock/HandTracking/handTracker.py
+++ sherlock/HandTracking/handTracker.py
@@ -28,48 +28,48 @@ class HandTracker:
 		return contour
 
 	def filterHSV(frame, min_threshold, max_threshold):
-	    mask = cv2.inRange(hsv, min_threshold, max_threshold)
-	    mask = cv2.medianBlur(mask, 5)
-	    return mask
+		mask = cv2.inRange(hsv, min_threshold, max_threshold)
+		mask = cv2.medianBlur(mask, 5)
+		return mask
 
 	def largestContour(contours):
 		largestContour = contours[0]
-	    largestArea = cv2.contourArea(largestContour)
-	    for contour in contours:
-	      if cv2.contourArea(contour) > largestArea:
-	        largestContour = contour
-	        largestArea = cv2.contourArea(contour)
-	    return largestContour
+		largestArea = cv2.contourArea(largestContour)
+		for contour in contours:
+			if cv2.contourArea(contour) > largestArea:
+				largestContour = contour
+				largestArea = cv2.contourArea(contour)
+		return largestContour
 	  
 	def findHand(frame):
-	  hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-	  min_threshold = [0, 51, 40]
-	  max_threshold = [19, 174, 121]
-	  mask = filterHSV(frame, min_threshold, max_threshold)
-	  filtered, contours, hierarchy = cv2.findContours(mask.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-	  handContour = largestContour(contours)
-	  return handContour
+		hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+		min_threshold = [0, 51, 40]
+		max_threshold = [19, 174, 121]
+		mask = filterHSV(frame, min_threshold, max_threshold)
+		filtered, contours, hierarchy = cv2.findContours(mask.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+		handContour = largestContour(contours)
+		return handContour
 
 	### HAND ANALYSIS
 
 	def isOpenPalm():
-	  return True
+		return True
 
 	def analyzeOpenPalm(simpleHandContour):
-	  # Find defects and finger locations
-	  # Find center of palm
-	  defects = cv2.convexHull(simpleHandContour, returnPoints = False)
-	  fingerLocations = cv2.convexHull(simpleHandContour)
-	  center = findCenter(simpleHandContour)
-	  
-	  return defects, fingerLocations, center
+		# Find defects and finger locations
+		# Find center of palm
+		defects = cv2.convexHull(simpleHandContour, returnPoints = False)
+		fingerLocations = cv2.convexHull(simpleHandContour)
+		center = findCenter(simpleHandContour)
+
+		return defects, fingerLocations, center
 
 	#NEED TO TEST TO FIND APPROPRIATE PERCENTAGE FOR CALCULATING EPSILON
 	def simplifyContour(complexContour):
-	  	percent = .1 # <- test values
+		percent = .1 # <- test values
 		epsilon = percent*cv2.arcLength(cnt,True)
-	    simpleContour = cv2.approxPolyDP(cnt,epsilon,True)
-	    return simpleContour
+		simpleContour = cv2.approxPolyDP(cnt,epsilon,True)
+		return simpleContour
 	  
 	"""
 	parameters: frame - cv2 Matrix 
